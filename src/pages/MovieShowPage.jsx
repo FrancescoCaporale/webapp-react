ed2dfa762be00586208c794053e43d7f0d61da12
FrancescoCaporale <frenk.caporale@gmail.com>
2026-02-25 18:25:10 +0100

    Usato map per rendere dinamicaa la pagina

--- src/pages/MovieShowPage.jsx
+++ src/pages/MovieShowPage.jsx
@@ -1,14 +1,44 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { api } from "../services/api";
 
 function MovieShowPage() {
     const { id } = useParams();
+    const [movie, setMovie] = useState(null);
+
+    useEffect(() => {
+        api.get(`/movies/${id}`)
+            .then((res) => {
+                setMovie(res.data);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }, [id]);
+
 
     return (
         <>
-            <h1>Dettaglio Film {id}</h1>
-            <p>Qui andranno i dettagli del film e le recensioni.</p>
+            <h1>{movie.title}</h1>
+            <p>{movie.description}</p>
+
+            <h3>Recensioni</h3>
+
+            {movie.reviews && movie.reviews.length > 0 ? (
+                <ul>
+                    {movie.reviews.map((review) => (
+                        <li key={review.id}>
+                            <strong>{review.name}</strong> - voto: {review.vote}
+                            <p>{review.text}</p>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>Nessuna recensione</p>
+            )}
         </>
     );
 }
+
 
 export default MovieShowPage;
